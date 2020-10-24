050a4c8de2382cec316a7b86f855388075e462d6
mahmudstat <almahmud.sbi@gmail.com>
2020-10-24 20:11:51 +0600

    Tidy data, add a line chart; untrack docx temp files (~*)

--- idm.R
+++ idm.R
@@ -2,3 +2,12 @@ library(tidyverse)
 
 idm <- read_csv("data/idm.csv") 
 
+View(idm)
+
+idm_tidy <- idm %>% pivot_longer(2:6, names_to = "region", values_to = "idm")
+
+## Bogura
+
+idm %>% ggplot(aes(x=Year, y = Bogura, group =1))+
+  geom_line(size=1.2, color="red")+
+  labs(y="De Martonne Aridiy Index")
