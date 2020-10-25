329bbfa729c7922fe8bcfe5adf6b618c59668c99
mahmudstat <almahmud.sbi@gmail.com>
2020-10-25 08:25:42 +0600

    Add a function to plot all regions

--- idm.R
+++ idm.R
@@ -9,5 +9,37 @@ idm_tidy <- idm %>% pivot_longer(2:6, names_to = "region", values_to = "idm")
 ## Bogura
 
 idm %>% ggplot(aes(x=Year, y = Bogura, group =1))+
-  geom_line(size=1.2, color="red")+
-  labs(y="De Martonne Aridiy Index")
+  geom_line(size=0.9, color="blue")+
+  labs(y="De Martonne Aridiy Index")+
+  geom_smooth()
+
+
+idm_tidy %>% filter(region=="Rajshahi") %>% 
+  ggplot(aes(x=Year, y = idm))+
+  geom_bar(stat="identity", fill="blue")+
+  labs(y="De Martonne Aridiy Index")+
+  geom_smooth(color="white")
+
+col <- c("#54C495", "#3BBFB2", "#4CB6C7", "#75A9D0", "#9E9ACA")
+
+dma <- function(r, col, col2){
+  idm_tidy %>% filter(region==r) %>% 
+    ggplot(aes(x=Year, y = idm, group =1))+
+    geom_line(size=0.8, color=col)+
+    labs(y="De Martonne Aridiy Index")
+  geom_smooth(color=col2)
+  
+  ggsave(filename = paste0("output/idm_",r,".png"), dpi = 300)
+  
+  ## Bar chart
+  idm_tidy %>% filter(region==r) %>% 
+    ggplot(aes(x=Year, y = idm))+
+    geom_bar(stat="identity", fill=col)+
+    labs(y="De Martonne Aridiy Index")+
+    geom_smooth(color=col2)
+  
+  ggsave(filename = paste0("output/idm_bar_",r,".png"), dpi = 300)
+}
+
+dma("Rajshahi", col = , col2 = "white")
+
